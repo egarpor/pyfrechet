9e155bfa98cf38a4c5ecd876c563029dca472fcd
egarpor <edugarpor@gmail.com>
2024-04-22 08:17:55 +0200

    Update job requirements

--- jesus.sh
+++
@@ -1,6 +0,0 @@
-
-module load python/3.9.9
-python simulations_S2/main_parallel.py
-git add .
-git commit -m "Simulations"
-git push
---
+++ job_jesus.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+module load python/3.9.9
+# cd /mnt/netapp2/Store_uni/home/usc/ei/ega/jesus/pyfrechet/simulations_S2/
+cd simulations_S2/
+lscpu
+python main_parallel.py $block
+git add .
+git commit -m "Simulations block $block"
+git push
+
+# for b in {1..60}
+# do
+#   sbatch --time=3:00:00 --cpus-per-task=50 --mem=10GB --mail-type=END,FAIL --mail-user=edgarcia@est-econ.uc3m.es --export=block=$b job_jesus.sh
+# done
