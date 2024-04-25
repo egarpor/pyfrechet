ae8872fad5eb7e262fa52ecfbbfb0de4d7e5eb44
Jesús Carretero Rodríguez <jesuscarretero2001@gmail.com>
2024-04-25 21:11:55 +0200

    Adjusted number of cores

--- simulations_T2/main_parallel.py
+++ simulations_T2/main_parallel.py
@@ -15,7 +15,7 @@ M=Torus(2)
 
 # By-blocks execution
 n_samples=len(os.listdir(os.path.join(os.getcwd(), 'data')))
-n_cores=4
+n_cores=50
 # n_cores=int(input('Introduce number of cores: '))
 n_blocks=n_samples/n_cores
 current_block=int(sys.argv[1])
