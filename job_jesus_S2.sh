68757238928263c6cb28b52fc58dbf60eb6bc851
Jesús Carretero Rodríguez <jesuscarretero2001@gmail.com>
2024-04-30 20:54:02 +0200

    Create job_jesus_S2.sh

---
+++ job_jesus_S2.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+module load python/3.9.9
+# cd /mnt/netapp2/Store_uni/home/usc/ei/ega/jesus/pyfrechet/simulations_S2/
+cd simulations_S2/
+lscpu
+python main_parallel.py $block
+cd ..
+git add .
+git commit -m "Simulations S2 block $block"
+git push
+
+# for b in {1..50}
+# do
+#   sbatch --time=3:00:00 --cpus-per-task=64 --mem=10GB --mail-type=END,FAIL --mail-user=edgarcia@est-econ.uc3m.es --export=block=$b --output="slurm-S2-block$b-jobid-%j.out" job_jesus_S2.sh
+# done
+
