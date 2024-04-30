89721ccb7a43118d63ab41b29a3b3303e885feed
Jesús Carretero Rodríguez <jesuscarretero2001@gmail.com>
2024-05-01 00:04:18 +0200

    Update for new T2 simulations
    Adjustments for the new fixed test size and new number of cores

--- job_jesus_T2.sh
+++ job_jesus_T2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 module load python/3.9.9
-# cd /mnt/netapp2/Store_uni/home/usc/ei/ega/jesus/pyfrechet/simulations_S2/
+# cd /mnt/netapp2/Store_uni/home/usc/ei/ega/jesus/pyfrechet/simulations_T2/
 cd simulations_T2/
 lscpu
 python main_parallel.py $block
@@ -10,8 +10,8 @@ git add .
 git commit -m "Simulations T2 block $block"
 git push
 
-# for b in {1..56}
+# for b in {1..50}
 # do
-#   sbatch --time=3:00:00 --cpus-per-task=50 --mem=10GB --mail-type=END,FAIL --mail-user=edgarcia@est-econ.uc3m.es --export=block=$b --output="slurm-T2-block$b-jobid-%j.out" job_jesus_T2.sh
+#   sbatch --time=4:00:00 --cpus-per-task=56 --mem=10GB --mail-type=END,FAIL --mail-user=edgarcia@est-econ.uc3m.es --export=block=$b --output="slurm-T2-block$b-jobid-%j.out" job_jesus_T2.sh
 # done
 
