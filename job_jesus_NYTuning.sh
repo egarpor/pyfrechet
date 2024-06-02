8d4fc13ddce29869fd43c8660905b39cbbefa4a9
Jesús Carretero Rodríguez <jesuscarretero2001@gmail.com>
2024-06-02 21:18:35 +0200

    Update job_jesus_NYTuning.sh

--- job_jesus_NYTuning.sh
+++ job_jesus_NYTuning.sh
@@ -4,11 +4,11 @@ module load python/3.9.9
 # cd /mnt/netapp2/Store_uni/home/usc/ei/ega/jesus/pyfrechet/notebooks/
 cd notebooks/
 lscpu
-python NY Taxi Data Tuning.py
+python NY_Taxi_Data_Tuning.py
 cd ..
 git add .
-git commit -m "NY Taxi Data Tuning 1"
+git commit -m "NY_Taxi_Data_Tuning 1"
 git push
 
 
-# sbatch --time=5:00:00 --cpus-per-task=64 --mem=10GB --mail-type=BEGIN,END,FAIL --mail-user=edgarcia@est-econ.uc3m.es --output="slurm-NYTuning-jobid.out" job_jesus_NYTuning.sh
+# sbatch --time=6:00:00 --cpus-per-task=64 --mem=10GB --mail-type=END,FAIL --mail-user=edgarcia@est-econ.uc3m.es --export=NYTuning --output="slurm-NYTuning-jobid-%j.out" job_jesus_NYTuning.sh
