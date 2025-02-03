c83251eba0930b641f0152437c01fa7a21569000
Diego <Diego@MacBook-Air.local>
2025-02-03 01:08:58 +0100

    The Fréchet mean respective to the affine-invariant distance is now the center of the regression function. The data simulation is now vectorized to make it faster

--- simulations_SPD/generator_samples.py
+++ simulations_SPD/generator_samples.py
@@ -1,66 +1,65 @@
 import sys, os; sys.path.append(os.path.dirname(os.getcwd())) 
 from typing import Union
 import numpy as np
-import pandas as pd
+from scipy.special import digamma
 import pickle
 from scipy.stats import wishart
 
-def w_1(t):
-    if t<=0.5:
-        return np.cos(np.pi*t)**2
-    else:
-        return 0
+def Sigma_t(t_array, Sigma_array):
+    """Provides an array with the matrices given by a regression model that interpolates between four matrices."""  
+    """The regression starts with Sigma_1 and then goes to Sigma_2 and Sigma_3 and ends in Sigma_4."""
     
-def w_3(t):
-    if t>=0.5:
-        return np.cos(np.pi*(1-t))**2
-    else:
-        return 0
-    
-Sigma_1=np.array([[1, -0.7],
-                  [-0.7, 0.5]])
-Sigma_2=np.array([[1, 0],
-                  [0, 1]])
-Sigma_3=np.array([[0.5, 0.4],
-                  [0.4, 1]])
+    # Define time intervals for interpolation
+    t_array = np.array(t_array)
+    t_array = t_array[:, None, None]
 
-def Sigma_true(t: float, 
-           Sigmas: tuple):
-    return w_1(t)*Sigmas[0]+(1-w_1(t)-w_3(t))*Sigmas[1]+w_3(t)*Sigmas[2]
+    # Return the interpolated matrices
+    return np.where(t_array < 0.5, np.cos(np.pi*t_array)**2 * Sigma_array[0] + (1 - np.cos(np.pi*(1-t_array))**2) * Sigma_array[1], 0) + np.where(t_array >= 0.5, (1 - np.cos(np.pi*t_array)**2) * Sigma_array[1] + np.cos(np.pi*(1-t_array))**2 * Sigma_array[2], 0)
+
+
 
 def sim_regression_matrices(Sigmas: tuple,
-                            size: int=1,
-                            random_state: Union[None, int]=None,
+                            t: np.array,
                             df: int=2):
-    np.random.seed(random_state)
-    sample_t=np.sort(np.random.uniform(size=size))
-    true_t=np.linspace(start=0, stop=1, num=size)
-    sample_Y=[(1/df)*wishart(df=df, scale=Sigma_true(sample_t[k], Sigmas)).rvs(size=1) for k in range(sample_t.size)]
-    true_Sigmat=[Sigma_true(true_t[k], Sigmas) for k in range(true_t.size)] 
+    t = np.array(t)
+    
+    #Simulate the time for regression (sample_t) and the true time (true_t)
+    q = Sigmas[0].shape[0]
 
-    return {'sample': (sample_t, sample_Y),
-            'true': (true_t, true_Sigmat)}
+    c_dq = 2 * np.exp((1 / q) * sum( digamma((df - np.arange(1, q + 1) + 1 ) / 2) ))
+    sigma_t = Sigma_t(t, Sigmas)
+    sample_Y = [1/c_dq*wishart( df=df, scale = sigma_t[k] ).rvs( size=1 ) for k in range(t.shape[0])]
+    return {'t': t, 'y': sample_Y}
+    
 
-number_samples=100
-test_size=100
-sample_sizes=[50, 100, 200, 500]
-sample_sizes=[size+test_size for size in sample_sizes]
-nus=[1.5, 2, 2.5, 3, 4, 5, 6]
+# Define the matrices to interpolate
+Sigma_1 = np.array([[1, -0.6],
+                  [-0.6, 0.5]])
+Sigma_2 = np.array([[1, 0],
+                  [0, 1]])
+Sigma_3 = np.array([[0.5, 0.4],
+                  [0.4, 1]])
+
+n_samples = 100
+sample_sizes = [50, 100, 200, 500]
+sample_sizes = [size for size in sample_sizes]
+dfs = [2, 2.5, 3, 3.5, 4, 5, 6]
 
 np.random.seed(1000)
 for sample_size in sample_sizes:
-    for nu in nus:
-        for k in range(1, number_samples+1):
-            sample=sim_regression_matrices((Sigma_1, Sigma_2, Sigma_3), 
-                                           size=sample_size,  
-                                           df=nu)
+    for df in dfs:
+        for k in range(1, n_samples+1):
+            sample_t = np.sort(np.random.uniform(size=sample_size))
+            sample = sim_regression_matrices(Sigmas = (Sigma_1, Sigma_2, Sigma_3), 
+                                           t = sample_t,  
+                                           df = df)
             
-            filename=os.path.join(os.getcwd(), 'data/SPD_Samp'+str(k)+'_N'+ \
-                                str(sample_size-test_size)+'_df'+ \
-                                str(nus.index(nu)+1)+'.pkl')
+            filename = os.path.join(os.getcwd(), 'simulations_SPD', 'data', 'SPD_Samp' + str(k)+'_N'+ \
+                                str(sample_size)+ '_df' + \
+                                str(dfs.index(df)+1)+'.pkl')
 
             with open(filename, 'wb') as f:
-                pickle.dump(sample, f)
+                np.save(sample, f)
             
 
 
