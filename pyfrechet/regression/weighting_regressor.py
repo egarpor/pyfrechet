c949cb20b37f423649cb6f797ecde1eda1e84a01
Diego <Diego@MacBook-Air.local>
2025-02-03 01:04:58 +0100

    the predictors can now handle matrices

--- pyfrechet/regression/weighting_regressor.py
+++ pyfrechet/regression/weighting_regressor.py
@@ -57,14 +57,27 @@ class WeightingRegressor(RegressorMixin, BaseEstimator, metaclass=ABCMeta):
         """
         return self.y_train_.frechet_mean(self.weights_for(x))
     
+    def _oob_predict_one(self, x):        
+        """
+        Make OOB prediction for just one new observation (requirement for oob_predict() method).
+        """
+        return self.y_train_.frechet_mean(self.oob_weights_for(x))
+    
     @abstractmethod
     def fit(self:T, X, y: MetricData) -> T:
         self.X_train_ = X
-        # y cannot have np.nan or np.inf when multi_output=True
-        # There is also a way of allowing np.nan (see sklearn help)
-        X, _ = check_X_y(X, y.data, multi_output=True)
+        if y.data.ndim ==1:
+            # y cannot have np.nan or np.inf when multi_output=True
+            # There is also a way of allowing np.nan (see sklearn help)
+            X, _ = check_X_y(X, y.data, multi_output=True, allow_nd=True)
+            
+        else:
+            if np.any(np.isnan(X)) or np.any(np.isinf(X)):
+                raise ValueError("X contains NaNs or infinite values")
+            if np.any(np.isnan(y.data)) or np.any(np.isinf(y.data)):
+                raise ValueError("y contains NaNs or infinite values")
+            
         self.y_train_ = y
-
 
         if self.precompute_distances:
             y.compute_distances()
@@ -75,10 +88,12 @@ class WeightingRegressor(RegressorMixin, BaseEstimator, metaclass=ABCMeta):
     def weights_for(self, x) -> np.ndarray:
         pass
 
+
+
     def predict(self, x):
         # Check if estimator has been fitted (.fit() has been applied), otherwise raise an error
         check_is_fitted(self)
-        x = check_array(x)
+        x = check_array(x) 
 
         # To allow (1,p) and (,p) shapes ('matrices' and 'vectors')
         if len(x.shape) == 1 or x.shape[0] == 1:
@@ -93,6 +108,25 @@ class WeightingRegressor(RegressorMixin, BaseEstimator, metaclass=ABCMeta):
                 y_pred[i,:] = self._predict_one(x[i,:])
             return MetricData(self.y_train_.M, y_pred)
         
+    def predict_matrix(self, x):
+        #TO DO: CHECK that AN ARRAY IS PASSED? UNEXPECTED BEHAVIOUR IF AN ARRAY IS NOT PASSED AND WE HAVE A MATRIX INSTEAD
+        # If y is a matrix:
+        # Check if estimator has been fitted (.fit() has been applied), otherwise raise an error
+        check_is_fitted(self)
+        x = check_array(x)
+
+        # To allow (1,p) and (,p) shapes ('matrices' and 'vectors')
+        if len(x.shape) == 1 or x.shape[0] == 1:
+            return self._predict_one(x)
+        else:
+            y0 = self._predict_one(x[0,:])
+            
+            y_pred = np.zeros((x.shape[0], y0.shape[0], y0.shape[1]))
+            y_pred[0,:,:] = y0
+            for i in range(1, x.shape[0]):
+                y_pred[i,:,:] = self._predict_one(x[i,:])
+            return MetricData(self.y_train_.M, y_pred)
+    
     def score(self, X, y: MetricData, sample_weight=None, force_finite=True):
         """Return the determination coefficient R^2 between target and fitted values.
         
