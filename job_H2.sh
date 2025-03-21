96bf68112ee88123ce68a750d67703c97237ee7f
Diego <Diego@MacBook-Pro.local>
2025-03-21 11:40:21 +0100

    shell script to do hyperboloid simulations in cluster

--- job_H2.sh
+++ job_H2.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#module load python/3.9.9
+module load python/3.9.9
 
-#lscpu
+lscpu
 python simulations_H2/H2_parallel.py $block
-# git add .
-# git commit -m "Simulations hyperboloid block $block"
-# git push
+git add .
+git commit -m "Simulations hyperboloid block $block"
+git push
 
 # for b in {1..20}
 # do
