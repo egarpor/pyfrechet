6484e7ad1a75672af2bd0f62e452c2b6d4817e4d
Diego <Diego@MacBook-Pro.local>
2025-03-21 13:37:30 +0100

    jobs to parallelize in cluster

---
+++ job_euc.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+module load python/3.9.9
+
+lscpu
+python simulations_sphere/sklearn_main_parallel.py $block
+git add .
+git commit -m "Simulations Euclidean block $block"
+git push
+
+#for b in {1..20}
+#do
+#  sbatch --time=4:00:00 --cpus-per-task=56 --mem=10GB --mail-type=END,FAIL --mail-user=dieserra@est-econ.uc3m.es --export=block=$b --output="slurm-sphere-block$b-jobid-%j.out" job_euc.sh
+#done
+
+# sbatch --time=0:10:00 --cpus-per-task=56 --mem=10GB --mail-type=BEGIN,END,FAIL --mail-user=dieserra@est-econ.uc3m.es --export=block=1 --output="slurm-h2-block1-jobid-%j.out" job_euc.sh
