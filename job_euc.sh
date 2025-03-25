a7c5098dd189fa5ef3658dd7f2762d2e5a513e0f
egarpor <edugarpor@gmail.com>
2025-03-25 11:35:30 +0100

    Update job_euc.sh

--- job_euc.sh
+++ job_euc.sh
@@ -12,5 +12,5 @@ git push
 #  sbatch --time=00:45:00 -n 1 --cpus-per-task=32 --mem=8GB --mail-type=END,FAIL --mail-user=edgarcia@est-econ.uc3m.es --export=block=$b --output="%j-euc-block$b.out" job_euc.sh
 #done
 
-# sbatch --time=00:45:00 -n 1 --cpus-per-task=32 --mem=8GB --mail-type=END,FAIL --mail-user=dieserra@est-econ.uc3m.es --export=block=1 --output="%j-euc-block1.out" job_euc.sh
+# sbatch --time=00:45:00 -n 1 --cpus-per-task=32 --mem=8GB --mail-type=END,FAIL --mail-user=edgarcia@est-econ.uc3m.es --export=block=1 --output="%j-euc-block1.out" job_euc.sh
 
