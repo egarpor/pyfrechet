b9b122f30a3b3ab6f555e0b8c78188210678726f
Diego <Diego@MacBook-Pro.local>
2025-03-25 12:45:06 +0100

    update jobs

--- job_sphere.sh
+++ job_sphere.sh
@@ -9,7 +9,7 @@ git push
 
 #for b in {1..20}
 #do
-#  sbatch --time=02:00:00 --cpus-per-task=32 --mem-per-cpu=8GB --mail-type=BEGIN,END,FAIL --mail-user=dieserra@est-econ.uc3m.es --export=block=$b --output="slurm-sphere-block$b-jobid-%j.out" job_sphere.sh
+#  sbatch --time=00:55:00 -n 1 --cpus-per-task=32 --mem=8GB --mail-type=BEGIN,END,FAIL --mail-user=dieserra@est-econ.uc3m.es --export=block=$b --output="slurm-sphere-block$b-jobid-%j.out" job_sphere.sh
 #done
 
-# sbatch --time=02:00:00 --cpus-per-task=32 --mem-per-cpu=8GB --mail-type=BEGIN,END,FAIL --mail-user=dieserra@est-econ.uc3m.es --export=block=1 --output="slurm-sphere-block1-jobid-%j.out" job_sphere.sh
+# sbatch --time=00:55:00 -n 1 --cpus-per-task=32 --mem=8GB --mail-type=BEGIN,END,FAIL --mail-user=dieserra@est-econ.uc3m.es --export=block=1 --output="%j-S2-block1.out" job_sphere.sh
