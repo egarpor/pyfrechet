2a4c9b454003a3b2cfbcb9d95cd2f84175d6c96f
Diego <Diego@MacBook-Pro.local>
2025-03-25 14:36:57 +0100

    do four blocks

--- job_euc.sh
+++ job_euc.sh
@@ -7,10 +7,9 @@ git add .
 git commit -m "Simulations Euclidean block $block"
 git push
 
-#for b in {1..20}
+#for b in {1..4}
 #do
 #  sbatch --time=00:45:00 -n 1 --cpus-per-task=32 --mem=8GB --mail-type=END,FAIL --mail-user=edgarcia@est-econ.uc3m.es --export=block=$b --output="%j-euc-block$b.out" job_euc.sh
 #done
 
 # sbatch --time=00:45:00 -n 1 --cpus-per-task=32 --mem=8GB --mail-type=END,FAIL --mail-user=edgarcia@est-econ.uc3m.es --export=block=1 --output="%j-euc-block1.out" job_euc.sh
-
