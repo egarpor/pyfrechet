750ae28ec2775327f59e9557155d0ca841108fe2
Diego <Diego@MacBook-Pro.local>
2025-03-27 12:35:15 +0100

    Update jobs

--- job_sphere.sh
+++ job_sphere.sh
@@ -6,7 +6,7 @@ git add .
 git commit -m "Simulations Sphere block $block"
 git push
 
-# for b in {3..20}
+# for b in {1..20}
 # do
 #   sbatch --time=01:22:00 -n 1 --cpus-per-task=32 --mem=16GB --mail-type=BEGIN,END,FAIL --mail-user=dieserra@est-econ.uc3m.es --export=block=$b --output="%j-sphere-block$b.out" job_sphere.sh
 # done
